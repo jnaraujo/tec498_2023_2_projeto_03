8a5abca34fa1bf756324eabc877754564049b6c2
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-27 15:15:44 -0300

    fix decod num

--- quartus/decodificador_num.v
+++ quartus/decodificador_num.v
@@ -2,79 +2,94 @@ module decodificador_num(
   sel,
   a, b, c, d, e, f, g, dp
 );
-  input [2:0] sel;
+  input [3:0] sel;
   output a, b, c, d, e, f, g, dp;
 
   wire w0, w1, w2, w3, w4, w5, w6, w7;
   wire w8, w9, w10, w11, w12, w13, w14, w15;
+  wire w16, w17, w18, w19, w20, w21, w22;
 
-  // A	  B	    C	    D	      E
-  // E sel[3] sel[2] sel[1] sel[0]
+  // A = sel[3]
+  // B = sel[2]
+  // C = sel[1]
+  // D = sel[0]
 
-  // A = (b AND NOT c AND NOT d) OR ( NOT b AND NOT c AND d)
-  and and0(w0, sel[2], ~sel[1], ~sel[0]); // b AND NOT c AND NOT d
-  and and1(w1, ~sel[2], ~sel[1], sel[0]); // NOT b AND NOT c AND d
-  or or0(a, w0, w1);
+  // A = (a AND b) OR (a AND c) OR ( NOT a AND NOT b AND NOT c AND d) OR (b AND NOT c AND NOT d)
+  and and0(w0, sel[3], sel[2]);
+  and and1(w1, sel[3], sel[1]);
+  and and2(w2, ~sel[3], ~sel[2], ~sel[1], sel[0]);
+  and and3(w3, sel[2], ~sel[1], ~sel[0]);
+  or or0(a, w0, w1, w2, w3);
 
-  // B = (b AND c AND NOT d) OR (b AND NOT c AND d)
-  and and2(w2, sel[2], sel[1], ~sel[0]); // b AND c AND NOT d
-  and and3(w3, sel[2], ~sel[1], sel[0]); // b AND NOT c AND d
-  or or1(b, w2, w3);
+  // B = (a AND b) OR (a AND c) OR (b AND c AND NOT d) OR (b AND NOT c AND d)
+  and and4(w4, sel[3], sel[2]);
+  and and5(w5, sel[3], sel[1]);
+  and and6(w6, sel[2], sel[1], ~sel[0]);
+  and and7(w7, sel[2], ~sel[1], sel[0]);
+  or or1(b, w4, w5, w6, w7);
 
-  // C = ( NOT b AND c AND NOT d)
-  and and4(w4, ~sel[2], sel[1], ~sel[0]); // NOT b AND c AND NOT d
-  or or2(c, w4);
+  // C = (a AND b) OR (a AND c) OR ( NOT b AND c AND NOT d)
+  and and8(w8, sel[3], sel[2]);
+  and and9(w9, sel[3], sel[1]);
+  and and10(w10, ~sel[2], sel[1], ~sel[0]);
+  or or2(c, w8, w9, w10);
 
-  // D = (b AND c AND d) OR (b AND NOT c AND NOT d) OR ( NOT b AND NOT c AND d)
-  and and5(w5, sel[2], sel[1], sel[0]); // b AND c AND d
-  and and6(w6, sel[2], ~sel[1], ~sel[0]); // b AND NOT c AND NOT d
-  and and7(w7, ~sel[2], ~sel[1], sel[0]); // NOT b AND NOT c AND d
-  or or3(d, w5, w6, w7);
+  // D = (a AND b) OR (a AND c) OR ( NOT a AND NOT b AND NOT c AND d) OR (b AND c AND d) OR (b AND NOT c AND NOT d)
+  and and11(w11, sel[3], sel[2]);
+  and and12(w12, sel[3], sel[1]);
+  and and13(w13, ~sel[3], ~sel[2], ~sel[1], sel[0]);
+  and and14(w14, sel[2], sel[1], sel[0]);
+  and and15(w15, sel[2], ~sel[1], ~sel[0]);
+  or or3(d, w11, w12, w13, w14, w15);
 
-  // E = (b AND NOT c) OR d
-  and and8(w8, sel[2], ~sel[1]); // b AND NOT c
-  or or4(e, w8, sel[0]);
+  // (a AND c) OR (b AND NOT c) OR d
+  and and16(w16, sel[3], sel[1]);
+  and and17(w17, sel[2], ~sel[1]);
+  or or4(e, w16, w17, sel[0]);
 
-  // F = ( NOT b AND c) OR ( NOT b AND d) OR (c AND d)
-  and and9(w9, ~sel[2], sel[1]); // NOT b AND c
-  and and10(w10, ~sel[2], sel[0]); // NOT b AND d
-  and and11(w11, sel[1], sel[0]); // c AND d
-  or or5(f, w9, w10, w11);
+  // (a AND b) OR ( NOT a AND NOT b AND d) OR ( NOT b AND c) OR (c AND d)
+  and and18(w18, sel[3], sel[2]);
+  and and19(w19, ~sel[3], ~sel[2], sel[0]);
+  and and20(w20, ~sel[2], sel[1]);
+  and and21(w21, sel[1], sel[0]);
+  or or5(f, w18, w19, w20, w21);
 
-  // G = (b AND c AND d) OR ( NOT b AND NOT c)
-  and and12(w12, sel[2], sel[1], sel[0]); // b AND c AND d
-  and and13(w13, ~sel[2], ~sel[1]); // NOT b AND NOT c
-  or or6(g, w12, w13);
+  // (a AND b) OR (a AND c) OR ( NOT a AND NOT b AND NOT c) OR (b AND c AND d)
+  and and22(w22, sel[3], sel[2]);
+  and and23(w23, sel[3], sel[1]);
+  and and24(w24, ~sel[3], ~sel[2], ~sel[1]);
+  and and25(w25, sel[2], sel[1], sel[0]);
+  or or6(g, w22, w23, w24, w25);
 
   // DP = 1
   assign dp = 1'b1;
 endmodule
 
 module TB_Decodificador_num();
-  reg [2:0] sel;
+  reg [3:0] sel;
 
   wire a, b, c, d, e, f, g, dp;
 
   decodificador_num decodificador_num(sel, a, b, c, d, e, f, g, dp);
 
   initial begin
-    sel = 3'b000; #10; // 0	0	0	0	0	0	1	1 - 0
-    sel = 3'b001; #10; // 1	0	0	1	1	1	1	1 - 1
-    sel = 3'b010; #10; // 0	0	1	0	0	1	0	1 - 2
-    sel = 3'b011; #10; // 0	0	0	0	1	1	0	1 - 3
-    sel = 3'b100; #10; // 1	0	0	1	1	0	0	1 - 4
-    sel = 3'b101; #10; // 0	1	0	0	1	0	0	1 - 5
-    sel = 3'b110; #10; // 0	1	0	0	0	0	0	1 - 6
-    sel = 3'b111; #10; // 0	0	0	1	1	1	1	1 - 7
-
-    sel = 3'b000; #10; // 1 1 1 1 1 1 1 1
-    sel = 3'b001; #10; // 1 1 1 1 1 1 1 1
-    sel = 3'b010; #10; // 1 1 1 1 1 1 1 1
-    sel = 3'b011; #10; // 1 1 1 1 1 1 1 1
-    sel = 3'b100; #10; // 1 1 1 1 1 1 1 1
-    sel = 3'b101; #10; // 1 1 1 1 1 1 1 1
-    sel = 3'b110; #10; // 1 1 1 1 1 1 1 1
-    sel = 3'b111; #10; // 1 1 1 1 1 1 1 1
+    sel = 4'b0000; #10; // 0	0	0	0	0	0	1	1 - 0
+    sel = 4'b0001; #10; // 1	0	0	1	1	1	1	1 - 1
+    sel = 4'b0010; #10; // 0	0	1	0	0	1	0	1 - 2
+    sel = 4'b0011; #10; // 0	0	0	0	1	1	0	1 - 3
+    sel = 4'b0100; #10; // 1	0	0	1	1	0	0	1 - 4
+    sel = 4'b0101; #10; // 0	1	0	0	1	0	0	1 - 5
+    sel = 4'b0110; #10; // 0	1	0	0	0	0	0	1 - 6
+    sel = 4'b0111; #10; // 0	0	0	1	1	1	1	1 - 7
+    sel = 4'b1000; #10; // 0	0	0	0	0	0	0	1 - 8
+    sel = 4'b1001; #10; // 0	0	0	0	1	0	0	1 - 9
+    sel = 4'b1010; #10; // 1	1 1 1	1	1	1	1 - #
+    sel = 4'b1011; #10; // 1	1 1 1	1	1	1	1 - #
+    sel = 4'b1100; #10; // 1	1 1 1	1	1	1	1 - #
+    sel = 4'b1101; #10; // 1	1 1 1	1	1	1	1 - #
+    sel = 4'b1110; #10; // 1	1 1 1	1	1	1	1 - #
+    sel = 4'b1111; #10; // 1	1 1 1	1	1	1	1 - #
+    
     
   end
 endmodule
