ab82b47f646a24aa4e1babe0033212e3b180ddcf
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-02-05 14:51:54 -0300

    fix contador

--- quartus/contador_0_99.v
+++ quartus/contador_0_99.v
@@ -1,8 +1,8 @@
 module contador_0_99(
-  clock, i, reset, auto_repor, reset_no_99,
+  clock, inc, reset, auto_repor, reset_no_99,
   M3, M2, M1, M0, S3, S2, S1, S0
   );
-  input clock, reset, auto_repor, i, reset_no_99;
+  input clock, reset, auto_repor, inc, reset_no_99;
   output M3, M2, M1, M0, S3, S2, S1, S0;
 
   wire clock_ccd, clock_ccd_d;
@@ -10,15 +10,18 @@ module contador_0_99(
   wire d_S3, d_S2, d_S1, d_S0;
   wire eh_nove_d, eh_nove_u, eh_noventa_nove;
   wire u_menor_5, d_zero, repor, reset_contador;
+  wire inc_dez;
 
   or orrst(reset_contador, reset, reset_no_99 && eh_noventa_nove);
 
-  and and0(clock_ccd, clock, ~eh_noventa_nove);
-  contador_cd ccd_u(clock_ccd, i, reset_contador, 1'b0, repor, u_S3, u_S2, u_S1, u_S0); // unidades
+  and and0(clock_ccd, ~clock, ~eh_noventa_nove);
+  contador_cd ccd_u(clock_ccd, inc, reset_contador, 1'b0, repor, u_S3, u_S2, u_S1, u_S0); // unidades
 
-  contador_cd ccd_d(clock_ccd_d, i, reset_contador, repor, 1'b0, d_S3, d_S2, d_S1, d_S0); // dezenas
+  FF_d(inc,clock,inc_dez);
 
-  mux_2x1 mux_2x1(u_S3, ~u_S3, i, clock_ccd_d);
+  mux_2x1 mux_2x1(u_S3, ~u_S3, inc_dez, clock_ccd_d);
+
+  contador_cd ccd_d(clock_ccd_d, inc_dez, reset_contador, repor, 1'b0, d_S3, d_S2, d_S1, d_S0); // dezenas
 
   eh_nove eh_noveU(u_S3, u_S2, u_S1, u_S0, eh_nove_u); // unidades eh nove
   eh_nove eh_noveD(d_S3, d_S2, d_S1, d_S0, eh_nove_d); // dezenas eh nove
